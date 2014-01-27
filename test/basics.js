5f657e21371a88bfffd49ba527be646e5409497d
Michiel B. de Jong <michiel@unhosted.org>
2014-01-27 05:27:38 +0000

    check spec version param

--- test/basics.js
+++ test/basics.js
@@ -1,7 +1,31 @@
 var remotestorageServer = require('../lib/remotestorage-server');
 
-exports['createServer creates a server'] = function (test) {
-  var server = remotestorageServer.createServer({}, {});
+exports['createServer accepts version string "draft-dejong-remotestorage-00"'] = function (test) {
+  var server = remotestorageServer.createServer('draft-dejong-remotestorage-00', {}, {});
   test.equal(!!server, true);
   test.done();
+};
+exports['createServer accepts version string "draft-dejong-remotestorage-01"'] = function (test) {
+  var server = remotestorageServer.createServer('draft-dejong-remotestorage-01', {}, {});
+  test.equal(!!server, true);
+  test.done();
+};
+exports['createServer supports version string "draft-dejong-remotestorage-02"'] = function (test) {
+  var server = remotestorageServer.createServer('draft-dejong-remotestorage-02', {}, {});
+  test.equal(!!server, true);
+  test.done();
+};
+exports['createServer supports version string "draft-dejong-remotestorage-03"'] = function (test) {
+  var server = remotestorageServer.createServer('draft-dejong-remotestorage-03', {}, {});
+  test.equal(!!server, true);
+  test.done();
+};
+exports['createServer throws an error for unknown version strings'] = function (test) {
+  var server;
+  try {
+    server = remotestorageServer.createServer('draft-dejong-remotestorage-04', {}, {});
+  } catch(e) {
+    test.equal(e.toString(), 'Error: unknown spec version "draft-dejong-remotestorage-04"');
+    test.done();
+  }
 };
